98f8ee232a877b5d974f5f1a65b765dcf7ac852d
exenza <massimo.berta@gmail.com>
2017-03-03 12:01:32 +0000

    made env.php otpional and replaceable with enviromantal veriables

--- api/db.php
+++ api/db.php
@@ -1,3 +1,9 @@
 <?php
-include "env.php";
+if((@include "env.php") === false){
+    exit('Fatel error missing env variables');
+    $db_host=$_ENV['db_host'];
+    $db_name=$_ENV['db_name'];
+    $db_user=$_ENV['db_user'];
+    $db_pass=$_ENV['db_pass'];
+}
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_pass);
