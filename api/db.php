0c85af5febb8d12e18ded73ceb60acb3c30939d1
exenza <massimo.berta@gmail.com>
2017-03-09 08:05:31 +0000

    fixed mispell

--- api/db.php
+++ api/db.php
@@ -1,9 +1,9 @@
 <?php
-if((@include "env.php") === false){
-    exit('Fatel error missing env variables');
-    $db_host=$_ENV['db_host'];
-    $db_name=$_ENV['db_name'];
-    $db_user=$_ENV['db_user'];
-    $db_pass=$_ENV['db_pass'];
+if((@include "env1.php") === false){
+    echo('Fatal error missing env variables');
+    $db_host=getenv('db_host');
+    $db_name=getenv('db_name');
+    $db_user=getenv('db_user');
+    $db_pass=getenv('db_pass');
 }
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_pass);
